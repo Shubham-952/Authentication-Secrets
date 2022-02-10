d6879b1f73fecae95a637ffcc85f529c7080e36a
Shubham Saraswat <shubhamsaraswat@Shubhams-MacBook-Air.local>
2022-02-10 11:46:24 +0530

    completed 3 level security by adding env variabes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -17,8 +18,7 @@ userSchema = new mongoose.Schema({
   password: String
 })
 
-secret = "Thisisourlittlesecret."
-
+secret = process.env.SECRET
 userSchema.plugin(encrypt, {
   secret: secret,
   encryptedFields: ["password"]
